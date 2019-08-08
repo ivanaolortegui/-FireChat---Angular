4c8bcc36a83b1fe61ad3a6e8336ae62d76680147
ivanaolortegui <olorteguimor@gmail.com>
2019-08-08 05:54:34 -0500

    Deploy en github 🚀

--- FireChat/src/environments/environment.prod.ts
+++ FireChat/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAcTfwXx5Xs9XEwZV0Pwlv2rWprkPKIXGo",
+    authDomain: "firechat-553f8.firebaseapp.com",
+    databaseURL: "https://firechat-553f8.firebaseio.com",
+    projectId: "firechat-553f8",
+    storageBucket: "",
+    messagingSenderId: "893655269410",
+    appId: "1:893655269410:web:806f661923b5421c"
+  }
 };
--- FireChat/src/environments/environment.ts
+++ FireChat/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyAcTfwXx5Xs9XEwZV0Pwlv2rWprkPKIXGo",
     authDomain: "firechat-553f8.firebaseapp.com",
